54b1a2f9299bf191953bd4dcf5732601a51bce3e
MUSA <asdf131996455@gmail.com>
2023-08-09 07:39:45 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,78 +1,88 @@
 #!/bin/bash
 
-# 安装必要的软件包
-function install_packages {
-  echo "正在安装必要的软件包..."
-  apt-get update
-  apt-get install -y p7zip p7zip-full p7zip-rar unzip unar unrar
-}
+# 安装所需软件
+sudo apt-get update
+sudo apt-get install p7zip p7zip-full p7zip-rar unzip unar unrar
 
-# 定义函数：解压 .rar 文件
-function extract_rar {
-  read -p "请输入解压密码: " rar_password
-  read -p "请输入目标目录: " target_directory
-  find "$target_directory" -name "*.rar" -execdir unrar x -p"$rar_password" -o+ {} \;
-  find "$target_directory" -name "*.7z.001" -execdir 7z x -p142150 {} \;
-}
+# 显示解压选项菜单
+echo "请选择操作:"
+echo "1. 解压zip文件"
+echo "2. 解压tar文件"
+echo "3. 解压7z文件"
+echo "4. 解压rar文件"
+echo "5. 退出"
 
-# 定义函数：删除指定文件类型
-function delete_files {
-  read -p "请输入目标目录: " target_directory
-  find "$target_directory" \( -name "*.txt" -o -name "*.url" -o -name "*.rar" -o -name "*.zip" -o -name "*.7z" \) -exec rm -rf {} \;
-}
+read choice
 
-# 定义函数：解压 .zip 文件
-function extract_zip {
-  read -p "请输入解压密码: " zip_password
-  read -p "请输入目标目录: " target_directory
-  find "$target_directory" -name "*.zip" -execdir unzip -P "$zip_password" {} \;
-}
-
-# 定义函数：解压 .7z 文件
-function extract_7z {
-  read -p "请输入解压密码: " _7z_password
-  read -p "请输入目标目录: " target_directory
-  find "$target_directory" -name "*.7z" -execdir 7z x -p"$_7z_password" {} \;
-}
-
-# 显示菜单选项
-function show_menu {
-  echo "选择要执行的操作："
-  echo "1. 安装必要的软件包"
-  echo "2. 解压 .rar 分卷包和 .7z 分卷包"
-  echo "3. 删除指定文件类型"
-  echo "4. 解压 .zip 分卷包"
-  echo "0. 退出"
-}
-
-# 处理用户输入的选项
-function handle_option {
-  read -p "请输入选项数字: " option
-  case $option in
+case $choice in
     1)
-      install_packages
-      ;;
-    2)
-      extract_rar
-      ;;
-    3)
-      delete_files
-      ;;
-    4)
-      extract_zip
-      ;;
-    0)
-      exit 0
-      ;;
-    *)
-      echo "无效的选项"
-      ;;
-  esac
-}
+        echo "请输入zip文件路径:"
+        read zip_path
 
-# 主循环
-while true; do
-  show_menu
-  handle_option
-  echo
-done
+        echo "是否是分卷包？(y/n)"
+        read is_split
+        if [ "$is_split" == "y" ]; then
+            echo "请输入分卷包序号（例如：'file.zip.001' 输入 '001'）:"
+            read split_num
+            unzip $zip_path.$split_num -d extracted_files
+        else
+            echo "请输入解压目录:"
+            read extract_dir
+            unzip $zip_path -d $extract_dir
+        fi
+        ;;
+    2)
+        echo "请输入tar文件路径:"
+        read tar_path
+
+        echo "是否有密码保护？(y/n)"
+        read has_password
+        if [ "$has_password" == "y" ]; then
+            echo "请输入密码:"
+            read password
+            tar -xvf $tar_path -C extracted_files --password=$password
+        else
+            echo "请输入解压目录:"
+            read extract_dir
+            tar -xvf $tar_path -C $extract_dir
+        fi
+        ;;
+    3)
+        echo "请输入7z文件路径:"
+        read sevenz_path
+
+        echo "是否有密码保护？(y/n)"
+        read has_password
+        if [ "$has_password" == "y" ]; then
+            echo "请输入密码:"
+            read password
+            7za x $sevenz_path -oextracted_files -p$password
+        else
+            echo "请输入解压目录:"
+            read extract_dir
+            7za x $sevenz_path -o$extract_dir
+        fi
+        ;;
+    4)
+        echo "请输入rar文件路径:"
+        read rar_path
+
+        echo "是否有密码保护？(y/n)"
+        read has_password
+        if [ "$has_password" == "y" ]; then
+            echo "请输入密码:"
+            read password
+            unrar x $rar_path extracted_files -p$password
+        else
+            echo "请输入解压目录:"
+            read extract_dir
+            unrar x $rar_path $extract_dir
+        fi
+        ;;
+    5)
+        exit 0
+        ;;
+    *)
+        echo "无效的选项"
+        ;;
+esac
